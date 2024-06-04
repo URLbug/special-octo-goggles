cdd5718b5187a9f58fbd67b915ba6d95301fd82b
Rui Carter <gemslove2@gmail.com>
2024-06-04 17:38:41 +0200

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,8 +18,18 @@
                     <li><a href="{{ route('home') }}">Главная</a></li>
                     <li><a href="{{ route('conf') }}">Регистрация участников конференции</a></li>
                     <li><a href="{{ route('gallery') }}">Фотогаллерея</a></li>
-                    <li><a href="#">Прайс-лист</a></li>
-                    <li><a href="#">Контакты</a></li>
+                    
+                    @if(!Auth::check())
+                        <li><a href="{{ route('reg') }}">Регистрация</a></li>
+                        <li><a href="{{ route('log') }}">Авторизация</a></li>
+                    @else
+                        <li><a href="{{ route('logout') }}">Выйти</a></li>
+                        <li><a href="{{ route('twitter') }}">Минитвитер</a></li>
+                        <li><a href="{{ route('test') }}">Тест</a></li>
+                    @endif
+
+                    <li><a href="{{ route('developer') }}">Об разработчике</a></li>
+
                 </ul>
             </nav>
         </header>
